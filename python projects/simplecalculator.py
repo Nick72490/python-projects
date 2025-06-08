3e1a11c310d65573c5e03fd05401735b18295bad
Nick <212217@student.aventus.nl>
2025-06-08 21:22:05 +0200

    fixed some issues in the calculator

--- python projects/simplecalculator.py
+++ python projects/simplecalculator.py
@@ -1,36 +1,39 @@
 def calculator():
-    while(True):
+    while True:
+        print("simple python calculator")
+        try:
+            a = int(input("number: "))
+            x = int(input("number: "))
+        except ValueError:
+            print("can only use numbers")
+            continue
 
-      print("simple python calculator")
-      try:
-          a = int(input("number: "))
-          x = int(input("number: "))
-      except ValueError:
-          print("can only use numbers")
-          continue
-      
-      print("1.+ plus")
-      print("2.- minus")
-      print("3./ divide")
-      print("4.* times")
-      print("type 'quit' to stop")
-      f = input()
+        while True:
+            print("1.+ plus")
+            print("2.- minus")
+            print("3./ divide")
+            print("4.* times")
+            print("type 'quit' to stop")
+            f = input()
 
-      if f == "+" or "1":
-          result = a + x
+            if f == "quit":
+                return  
 
-      if f == "-" or "2":
-          result = a - x
+            if f in ("+", "1"):
+                result = a + x
+                break
+            elif f in ("-", "2"):
+                result = a - x
+                break
+            elif f in ("/", "3"):
+                result = a / x
+                break
+            elif f in ("*", "4"):
+                result = a * x
+                break
+            else:
+                print("can only use the given options")
+                continue
 
-      if f == "/" or "3":
-          result = a / x
-      
-      if f == "*" or "4":
-          result = a * x
-
-      if f == "quit":
-          break  
-
-      print(result)
+        print("Result:", result)
 calculator()
-    
